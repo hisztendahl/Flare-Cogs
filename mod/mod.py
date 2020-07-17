85f13f852f7d870db260fcefb273b9bbc9d14622
flare <flare2399@gmail.com>
2020-07-17 20:02:49 +0100

    userinfo intergrated with mod

--- mod/mod.py
+++ mod/mod.py
@@ -1,23 +1,21 @@
-import asyncio
-import logging
-from datetime import datetime, timedelta
-from typing import Optional
-
 import discord
 from redbot.cogs.mod import Mod as ModClass
-from redbot.core import Config, checks, commands, modlog
-from redbot.core.commands.converter import TimedeltaConverter
-from redbot.core.utils.chat_formatting import humanize_list, humanize_timedelta, inline
-from redbot.core.utils.mod import is_allowed_by_hierarchy
-from redbot.core.utils.predicates import MessagePredicate
+from redbot.core import bank, commands
+from redbot.core.utils import AsyncIter
+from redbot.core.utils.chat_formatting import humanize_list, humanize_number
+from redbot.core.utils.common_filters import (
+    escape_spoilers_and_mass_mentions,
+    filter_invites,
+    filter_various_mentions,
+)
 
-log = logging.getLogger("red.flarecogs.mod")
+from .flags import EMOJIS, discord_py
 
 
 class Mod(ModClass):
-    """Mod with timed mute."""
+    """Mod with userinfo ehhancements."""
 
-    __version__ = "1.1.5"
+    __version__ = "1.0.0"
 
     def format_help_for_context(self, ctx):
         """Thanks Sinbad."""
@@ -27,211 +25,182 @@ class Mod(ModClass):
     def __init__(self, bot):
         super().__init__(bot)
         self.bot = bot
-        self.__config = Config.get_conf(
-            self, identifier=95932766180343808, force_registration=True
-        )
-        defaultsguild = {"muterole": None, "respect_hierarchy": True}
-        defaults = {"muted": {}}
-        self.__config.register_guild(**defaultsguild)
-        self.__config.register_global(**defaults)
-        self.loop = bot.loop.create_task(self.unmute_loop())
+        self.status_emojis = {
+            "mobile": discord.utils.get(self.bot.emojis, id=725387684113023057),
+            "online": discord.utils.get(self.bot.emojis, id=724950463417548890),
+            "away": discord.utils.get(self.bot.emojis, id=724950462729551883),
+            "dnd": discord.utils.get(self.bot.emojis, id=724950462499127338),
+            "offline": discord.utils.get(self.bot.emojis, id=724950462746460271),
+            "streaming": discord.utils.get(self.bot.emojis, id=724950551900717066),
+        }
 
-    # Removes main mods mute commands.
-    voice_mute = None
-    channel_mute = None
-    guild_mute = None
-    unmute_voice = None
-    unmute_channel = None
-    unmute_guild = None
-    # ban = None # TODO: Merge hackban and ban.
+    # Removes main userinfo command.
+    userinfo = None
 
-    def cog_unload(self):
-        self.loop.cancel()
-
-    async def unmute_loop(self):
-        while True:
-            muted = await self.__config.muted()
-            for guild in muted:
-                for user in muted[guild]:
-                    if datetime.fromtimestamp(muted[guild][user]["expiry"]) < datetime.now():
-                        await self.unmute(user, guild)
-            await asyncio.sleep(15)
-
-    async def unmute(self, user, guildid, *, moderator: discord.Member = None):
-        guild = self.bot.get_guild(int(guildid))
-        if guild is None:
-            return
-        mutedroleid = await self.__config.guild(guild).muterole()
-        muterole = guild.get_role(mutedroleid)
-        member = guild.get_member(int(user))
-        if member is not None:
-            if moderator is None:
-                await member.remove_roles(muterole, reason="Mute expired.")
-                log.info("Unmuted {} in {}.".format(member, guild))
-            else:
-                await member.remove_roles(muterole, reason="Unmuted by {}.".format(moderator))
-                log.info("Unmuted {} in {} by {}.".format(member, guild, moderator))
-            await modlog.create_case(
-                self.bot,
-                guild,
-                datetime.utcnow(),
-                "sunmute",
-                member,
-                moderator,
-                "Automatic Unmute" if moderator is None else None,
-            )
-        else:
-            log.info("{} is no longer in {}, removing from muted list.".format(user, guild))
-        async with self.__config.muted() as muted:
-            if user in muted[guildid]:
-                del muted[guildid][user]
-
-    async def create_muted_role(self, guild):
-        muted_role = await guild.create_role(
-            name="Muted", reason="Muted role created for timed mutes."
-        )
-        await self.__config.guild(guild).muterole.set(muted_role.id)
-        o = discord.PermissionOverwrite(send_messages=False, add_reactions=False, connect=False)
-        for channel in guild.channels:
-            mr_overwrite = channel.overwrites.get(muted_role)
-            if not mr_overwrite or o != mr_overwrite:
-                await channel.set_permissions(
-                    muted_role,
-                    overwrite=o,
-                    reason="Ensures that Muted users won't be able to talk here.",
-                )
-
-    @checks.mod_or_permissions(manage_roles=True)
-    @checks.bot_has_permissions(manage_roles=True)
-    @commands.group(invoke_without_command=True)
-    async def mute(
-        self,
-        ctx,
-        users: commands.Greedy[discord.Member],
-        duration: Optional[TimedeltaConverter] = None,
-        *,
-        reason: str = None,
-    ):
-        """Mute users."""
-        if not users:
-            return await ctx.send_help()
-        if duration is None:
-            duration = timedelta(minutes=10)
-        duration_seconds = duration.total_seconds()
+    @commands.command()
+    @commands.guild_only()
+    @commands.bot_has_permissions(embed_links=True)
+    async def userinfo(self, ctx, *, user: discord.Member = None):
+        """Show userinfo with some more detail."""
+        author = ctx.author
         guild = ctx.guild
-        roleid = await self.__config.guild(guild).muterole()
-        if roleid is None:
-            await ctx.send(
-                "There is currently no mute role set for this server. If you would like one to be automatically setup then type yes, otherwise type no then one can be set via {}mute roleset <role>".format(
-                    ctx.prefix
-                )
-            )
-            try:
-                pred = MessagePredicate.yes_or_no(ctx, user=ctx.author)
-                msg = await ctx.bot.wait_for("message", check=pred, timeout=60)
-            except asyncio.TimeoutError:
-                return await ctx.send("Alright, cancelling the operation.")
 
-            if pred.result:
-                await msg.add_reaction("\N{WHITE HEAVY CHECK MARK}")
-                await self.create_muted_role(guild)
-                roleid = await self.__config.guild(guild).muterole()
+        if not user:
+            user = author
+        sharedguilds = {
+            guild async for guild in AsyncIter(self.bot.guilds) if user in guild.members
+        }
+        roles = user.roles[-1:0:-1]
+        names, nicks = await self.get_names_and_nicks(user)
+
+        joined_at = user.joined_at
+        since_created = (ctx.message.created_at - user.created_at).days
+        if joined_at is not None:
+            since_joined = (ctx.message.created_at - joined_at).days
+            user_joined = joined_at.strftime("%d %b %Y %H:%M")
+        else:
+            since_joined = "?"
+            user_joined = "Unknown"
+        user_created = user.created_at.strftime("%d %b %Y %H:%M")
+        voice_state = user.voice
+        member_number = (
+            sorted(guild.members, key=lambda m: m.joined_at or ctx.message.created_at).index(user)
+            + 1
+        )
+
+        created_on = "{}\n({} days ago)".format(user_created, since_created)
+        joined_on = "{}\n({} days ago)".format(user_joined, since_joined)
+        if user.is_on_mobile():
+            statusemoji = (
+                self.status_emojis["mobile"]
+                if self.status_emojis["mobile"]
+                else "\N{MOBILE PHONE}"
+            )
+        elif any(a.type is discord.ActivityType.streaming for a in user.activities):
+            statusemoji = (
+                self.status_emojis["streaming"]
+                if self.status_emojis["streaming"]
+                else "\N{LARGE PURPLE CIRCLE}"
+            )
+        elif user.status.name == "online":
+            statusemoji = (
+                self.status_emojis["online"]
+                if self.status_emojis["online"]
+                else "\N{LARGE GREEN CIRCLE}"
+            )
+        elif user.status.name == "offline":
+            statusemoji = (
+                self.status_emojis["offline"]
+                if self.status_emojis["offline"]
+                else "\N{MEDIUM WHITE CIRCLE}"
+            )
+        elif user.status.name == "dnd":
+            statusemoji = (
+                self.status_emojis["dnd"] if self.status_emojis["dnd"] else "\N{LARGE RED CIRCLE}"
+            )
+        elif user.status.name == "idle":
+            statusemoji = (
+                self.status_emojis["away"]
+                if self.status_emojis["away"]
+                else "\N{LARGE ORANGE CIRCLE}"
+            )
+        activity = "Chilling in {} status".format(user.status)
+        status_string = self.get_status_string(user)
+
+        if roles:
+
+            role_str = ", ".join([x.mention for x in roles])
+            # 400 BAD REQUEST (error code: 50035): Invalid Form Body
+            # In embed.fields.2.value: Must be 1024 or fewer in length.
+            if len(role_str) > 1024:
+                # Alternative string building time.
+                # This is not the most optimal, but if you're hitting this, you are losing more time
+                # to every single check running on users than the occasional user info invoke
+                # We don't start by building this way, since the number of times we hit this should be
+                # infintesimally small compared to when we don't across all uses of Red.
+                continuation_string = (
+                    "and {numeric_number} more roles not displayed due to embed limits."
+                )
+
+                available_length = 1024 - len(continuation_string)  # do not attempt to tweak, i18n
+
+                role_chunks = []
+                remaining_roles = 0
+
+                for r in roles:
+                    chunk = f"{r.mention}, "
+                    chunk_size = len(chunk)
+
+                    if chunk_size < available_length:
+                        available_length -= chunk_size
+                        role_chunks.append(chunk)
+                    else:
+                        remaining_roles += 1
+
+                role_chunks.append(continuation_string.format(numeric_number=remaining_roles))
+
+                role_str = "".join(role_chunks)
+
+        else:
+            role_str = None
+
+        data = discord.Embed(
+            description=(status_string or activity) + f"\n\n{len(sharedguilds)} shared servers.",
+            colour=user.colour,
+        )
+
+        data.add_field(name="Joined Discord on", value=created_on)
+        data.add_field(name="Joined this server on", value=joined_on)
+        if role_str is not None:
+            data.add_field(name="Roles", value=role_str, inline=False)
+        if names:
+            # May need sanitizing later, but mentions do not ping in embeds currently
+            val = filter_invites(", ".join(names))
+            data.add_field(name="Previous Names", value=val, inline=False)
+        if nicks:
+            # May need sanitizing later, but mentions do not ping in embeds currently
+            val = filter_invites(", ".join(nicks))
+            data.add_field(name="Previous Nicknames", value=val, inline=False)
+        if voice_state and voice_state.channel:
+            data.add_field(
+                name="Current voice channel",
+                value="{0.mention} ID: {0.id}".format(voice_state.channel),
+                inline=False,
+            )
+        data.set_footer(text="Member #{} | User ID: {}".format(member_number, user.id))
+
+        name = str(user)
+        name = " ~ ".join((name, user.nick)) if user.nick else name
+        name = filter_invites(name)
+
+        avatar = user.avatar_url_as(static_format="png")
+        data.title = f"{statusemoji} {name}"
+        data.set_thumbnail(url=avatar)
+
+        flags = await discord_py(user)
+        badges = ""
+        for badge in sorted(flags):
+            if badge == "verified_bot":
+                emoji1 = discord.utils.get(self.bot.emojis, id=EMOJIS["verified_bot"])
+                emoji2 = discord.utils.get(self.bot.emojis, id=EMOJIS["verified_bot2"])
+                if emoji1:
+                    emoji = f"{emoji1}{emoji2}"
+                else:
+                    emoji = None
             else:
-                await msg.add_reaction("\N{WHITE HEAVY CHECK MARK}")
-                return
-        mutedrole = guild.get_role(roleid)
-        if mutedrole is None:
-            return await ctx.send(
-                f"The mute role for this server is invalid. Please set one up using {ctx.prefix}mute roleset <role>."
-            )
-        completed = []
-        failed = []
-        async with self.__config.muted() as muted:
-            if str(ctx.guild.id) not in muted:
-                muted[str(ctx.guild.id)] = {}
-            for user in users:
-                if user == ctx.author:
-                    failed.append(f"{user} - Self harm is bad.")
-                    continue
-                if not await is_allowed_by_hierarchy(
-                    self.bot, self.__config, guild, ctx.author, user
-                ):
-                    failed.append(
-                        f"{user} - You are not higher than this user in the role hierarchy"
-                    )
-                    continue
-                if guild.me.top_role <= user.top_role or user == guild.owner:
-                    failed.append(
-                        f"{user} - Discord hierarcy rules prevent you from muting this user."
-                    )
-                    continue
-                await user.add_roles(
-                    mutedrole,
-                    reason="Muted by {} for {}{}".format(
-                        ctx.author,
-                        humanize_timedelta(timedelta=duration),
-                        f" | Reason: {reason}" if reason is not None else "",
-                    ),
-                )
-                expiry = datetime.now() + timedelta(seconds=duration_seconds)
-                muted[str(ctx.guild.id)][str(user.id)] = {
-                    "time": datetime.now().timestamp(),
-                    "expiry": expiry.timestamp(),
-                }
-                await modlog.create_case(
-                    ctx.bot,
-                    ctx.guild,
-                    ctx.message.created_at,
-                    "smute",
-                    user,
-                    ctx.author,
-                    reason,
-                    expiry,
-                )
-                log.info(
-                    f"{user} muted by {ctx.author} in {ctx.guild} for {humanize_timedelta(timedelta=duration)}"
-                )
-                completed.append(user)
-        msg = "{}".format("\n**Reason**: {}".format(reason) if reason is not None else "")
-        if completed:
-            await ctx.send(
-                f"`{humanize_list(list(map(inline, [str(x) for x in completed])))}` has been muted for {humanize_timedelta(timedelta=duration)}.{msg}"
-            )
-        if failed:
-            failemsg = "\n{}".format("\n".join(failed))
-            await ctx.send(
-                f"{len(failed)} user{'s' if len(failed) > 1 else ''} failed to be muted for the following reasons.{failemsg}"
-            )
-
-    @checks.admin_or_permissions(manage_roles=True)
-    @mute.command()
-    async def roleset(self, ctx, role: discord.Role):
-        """Set a mute role."""
-        await self.__config.guild(ctx.guild).muterole.set(role.id)
-        await ctx.send("The muted role has been set to {}".format(role.name))
-
-    @checks.mod_or_permissions(manage_roles=True)
-    @checks.bot_has_permissions(manage_roles=True)
-    @commands.group(invoke_without_command=True, name="unmute")
-    async def _unmute(self, ctx, users: commands.Greedy[discord.Member]):
-        """Unmute users."""
-        muted = await self.__config.muted()
-        for user in users:
-            if str(ctx.guild.id) not in muted:
-                return await ctx.send("There is nobody currently muted in this server.")
-            await self.unmute(str(user.id), str(ctx.guild.id), moderator=ctx.author)
-            await ctx.tick()
-
-    @checks.mod_or_permissions(manage_roles=True)
-    @mute.command(name="list")
-    async def _list(self, ctx):
-        """List those who are muted."""
-        muted = await self.__config.muted()
-        guildmuted = muted.get(str(ctx.guild.id))
-        if guildmuted is None:
-            return await ctx.send("There is currently nobody muted in {}".format(ctx.guild))
-        msg = ""
-        for user in guildmuted:
-            expiry = datetime.fromtimestamp(guildmuted[user]["expiry"]) - datetime.now()
-            msg += f"{self.bot.get_user(int(user)).mention} is muted for {humanize_timedelta(timedelta=expiry)}\n"
-        await ctx.maybe_send_embed(msg if msg else "Nobody is currently muted.")
+                emoji = discord.utils.get(self.bot.emojis, id=EMOJIS[badge])
+            if emoji:
+                badges += f"{emoji} {badge.replace('_', ' ').title()}\n"
+            else:
+                badges += f"\N{BLACK QUESTION MARK ORNAMENT}\N{VARIATION SELECTOR-16} {badge.replace('_', ' ').title()}\n"
+        if badges:
+            data.add_field(name="Badges", value=badges)
+        bankstat = f"**Bank**: {str(humanize_number(await bank.get_balance(user)))} {await bank.get_currency_name(ctx.guild)}\n"
+        if "Unbelievaboat" in self.bot.cogs:
+            cog = self.bot.get_cog("Unbelievaboat")
+            state = await cog.walletdisabledcheck(ctx)
+            if not state:
+                balance = await cog.walletbalance(user)
+                bankstat += f"**Wallet**: {str(humanize_number(balance))} {await bank.get_currency_name(ctx.guild)}"
+        data.add_field(name="Balance", value=bankstat)
+        await ctx.send(embed=data)
